e48ca3c5422e09da516c2d6b9346423613e08f51
indira923 <108821566+indira923@users.noreply.github.com>
2022-07-07 11:40:36 -0400

    Add files via upload

--- variables.py
+++ variables.py
@@ -18,32 +18,30 @@ print(message)
 
 print("\n------------------- Challenge 1 -------------------\n")
 
-#print(f"Hi! My name is {name}")
-#print(f"I'm in the {grade}th grade.")
-#print(f"A fun fact about me is that {funFact}")
+name = "Abby"
+grade = 6
+funFact = "left handed"
+
+print(f"Hi! My name is {name}")
+print(f"I'm in the {grade}th grade.")
+print(f"A fun fact about me is that {funFact}")
 
 
-# **** Challenge 1: Problem 1 ****
-# Initialize the variables used in the {} with your data for the three statements above! 
-# Hint: Also think about where you would initialize those variables
+
 
 
 # **** Upchallenge! ****
 # Can you also print out the data type of the variables? 
 
 
-
-
-# **** Challenge 1: Problem 2 ****
-# Create a string for the different lines of the Happy Birthday Song
-# Print out the song using the strings you declared.
-
-
-
+print ("happy birthday to you")
+print ("happy birthday to you")
+print ("happy birthday to you")
 
 # **** Challenge 1: Problem 3 ****
 # Print out a string that has brackets in it.
-
+food = "burned cookie"
+print(f"I am a {food}")
 
 
 
@@ -58,36 +56,39 @@ print("\n------------------- Challenge 2 -------------------\n")
 # Here are some variables to get you started
 num1 = 5
 num2 = 10
-
+num3 = 15
 # **** Challenge 2: Problem 1 ****
 # Store the sum of num1 and num2 in a variable.
 # Print the sum.
 
-
+print(num3)
 # **** Challenge 2: Problem 2 ****
 # Store the difference between sum (above) and 7 in a variable.
 # Print the difference.
-
+num4 = 7
+print(num3-num4)
 
 # **** Challenge 2: Problem 3 ****
 # Store the product of the difference and 3 in a variable.
 # Print the product.
-
-
+num5 = 3
+print(num4*num5)
 # **** Challenge 2: Problem 4 ****
 # Store the power of the product squared in a variable.
 # Print the power.
-
-
+num6 = 21
+print(21**2)
 # **** Challenge 2: Problem 5 ****
 # Store the quotient of the power divided by 4 in a variable.
 # Print the quotient.
-
+num7 = 441
+print(441/4)
 
 # **** Challenge 2: Problem 6 ****
 # Store the remainder of the quotient divided by 2 in a variable.
 # Print the remainder.
-
+num8 = 110.5
+print (110.5%2)
 
 # **** Upchallenge! ****
 # Given a variable that stores a number, print the values that come right before and right after.
