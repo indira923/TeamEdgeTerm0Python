14053b73be750bea2b073549b33e766263399e67
indira923 <indira.hewitt@csedge.org>
2022-07-14 06:34:36 -0700

    lab 6 challenges - lists

--- lists.py
+++ lists.py
@@ -28,14 +28,14 @@ names = ["Julian", "Wolf", "Alex", "Steph", "Alessandro"]
 print("names: " + str(names))  
 
 #-->TODO: Declare another list called friends with at least 5 strings inside (if you don't have 5 friends make them up!)
-
+names = ["koko, alex, peter, sophie, bobby"]
 
 #this list holds numbers
 numbers = [12.9, 23.4 , 100, 3.1415 , 500, 1.20]
 print("numbers: " + str(numbers)) 
 
 #-->TODO: Declare another list and add in at least 5 numbers. Why five? I don't know. It just feels right.
-
+numbers = ["15, 23, 45, 61, 94"]
 
 
 #this list has mixed data types. It's allowed in Python!
@@ -43,10 +43,10 @@ random_stuff = ["Aardvark", True, False, 1.23, "Grandpa"]
 print("random: " + str(random_stuff))
 
 #-->TODO: Declare and log a list filled with the first 5 things that come into your head, booleans, Strings, numbers are all cool,
-
+things = ["cat", False, tree, 2.5, 8]
 
 #-->TODO: Declare and log two more lists with whatever you want. 
-
+things2 = ["dog", True, 4.5, "snail",2.48]
 
 print("------------------- CHALLENGE 2 -------------------")
  
@@ -54,18 +54,22 @@ print("------------------- CHALLENGE 2 -------------------")
 print("The first name is " + names[0])
 
 #-->TODO: Print the name of your best friend from your friends list
-
+print("koko")
 
 #-->TODO: Print the first AND last elements of any list you made, or make a brand new one.
-
+print("dog", 2.48)
 
 print("------------------- CHALLENGE 3 -------------------")
 #this code changes the value of the second element of the names list, then we print the list:
 names[1] = "Alyssa"
-print(names)
+print()
 
 #-->TODO: Replace your friends! Modify the list to replace any or all of your friends with new ones.
-
+names[2] = "koko"
+names[3] = "alex"
+names[4] = "peter"
+names[5] = "sophie"
+names[6] = "joshua"
 
 #The code below uses the times_ten() function to multiply the first element in our list by 10:
 def times_ten(number):
@@ -76,11 +80,19 @@ numbers[0] = times_ten(numbers[0])
 print(numbers)
 
 #-->TODO: Write another function that multiplies a number by 1000 and print the list, as above 
-
+def mult():
+    a=5
+    b=1000
+    5*1000
+    
 
 
 #-->TODO: Replace your random list elements with anything you want, using the index. 
-
+things2[1] = "dog"
+things2[2] = False
+things2[3] = 4.5
+things2[4] = "snail"
+things2[5] = 2.48 
 print("------------------- CHALLENGE 4 -------------------")
 
 #As it turns out, you can also store lists within lists! Declare them and store them as variables.
@@ -90,8 +102,9 @@ parent_list = [child_list_1, child_list_2]
 print("This list has babies: " + str(parent_list))
 
 #-->TODO: Store and print all the lists we have worked on thus far in a new parent list
-
-
+things = ["cat", False, tree, 2.5, 8]
+things2 = ["dog", True, 4.5, "snail",2.48  ]
+parent_list = [things, things2]
 print("------------------- CHALLENGE 5 -------------------")
 
 #We can add elements into a list wihtout replacing anything. Using append() adds an element to the END of the list:
@@ -102,9 +115,9 @@ movies.append("Black Panther")
 print("Movies now has: " + str(movies))
 
 #-->TODO: Declare a list with 5 favorite songs
-
+songs_list["day1", "house of cards", "loretta", "rivers in the dessert","cool kids"]
 #-->TODO: Add 2-3 more songs using .append() and print both before and after.
-
+songs_list.append():("life will change", "too late", "two step")
 #We can also remove elements using .pop(), which removes the last element or the element at the given index. You can store it after it comes out:
 cities = ["New York", "Oakland", "Las Vegas", "Topeka"]
 print("cities: " + str(cities))
@@ -112,7 +125,7 @@ unwanted_city = cities.pop()
 print("unwanted city: " + str(unwanted_city))
 
 #-->TODO: remove your last song using .pop() and print the removed element as above
-
+.pop(3)
 #Note: there are more methods to remove and modify list elements. We will cover those later
 
 print("------------------- CHALLENGE 6 -------------------")
